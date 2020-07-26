d2fcdd58335cfce41001887a27dc19b3fddcdb13
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2020-07-25 23:25:46 -0500

    Code review and security

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '6049e8839f0f460683a597fe837ac23d'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('unable to work with api'))
+}
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0])
+        })
+        .catch(err => res.status(400).json('unable to se entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
